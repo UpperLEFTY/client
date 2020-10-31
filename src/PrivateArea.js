78bffc73c15fa8217582cea235c939ad35e6bbdb
UpperLEFTY <upperlefty@zoho.eu>
2020-10-31 14:01:47 -0700

    adding apollo boost

--- src/PrivateArea.js
+++ src/PrivateArea.js
@@ -1,13 +1,41 @@
 import React from 'react';
+import { gql } from 'apollo-boost';
+import { Query } from 'react-apollo';
 import Cookies from 'js-cookie';
 import { navigate } from '@reach/router';
 
 const PrivateArea = () => {
-  if (!Cookies.get('token')) {
+  if (!Cookies.get('signedin')) {
     navigate('/');
   }
 
-  return <div>Private area!</div>;
+  return (
+    <Query
+      query={gql`
+        {
+          todos {
+            id
+            name
+          }
+        }
+      `}
+    >
+      {({ loading, error, data }) => {
+        if (loading) return <p>Loading...</p>;
+        if (error) {
+          navigate('/');
+          return <p></p>;
+        }
+        return (
+          <ul>
+            {data.todos.map((item) => (
+              <li key={item.id}>{item.name}</li>
+            ))}
+          </ul>
+        );
+      }}
+    </Query>
+  );
 };
 
 export default PrivateArea;
